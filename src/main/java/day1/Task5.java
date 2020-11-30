284ba86b47710c2356eaf84a4a40afc1472619a4
zhhhigalov <zhigalovmi@gmail.com>
2020-11-30 21:25:59 +0300

    complete day1 tasks

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -3,5 +3,8 @@ package day1;
 public class Task5 {
     public static void main(String[] args) {
 
+        for (int year = 1980; year <= 2020; year = year + 4) {
+            System.out.println("Олимпиада " + year + " года");
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -3,5 +3,9 @@ package day1;
 public class Task6 {
     public static void main(String[] args) {
 
+        int k = 9;
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " x " + k + " = " + i * k);
+        }
     }
 }
