07ac99a97eedf05e32a301551d42f87b25f2ab1a
zhhhigalov <zhigalovmi@gmail.com>
2020-12-05 03:16:55 +0300

    complete day4 tasks fixes

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -8,9 +8,9 @@ public class Task3 {
         Random rand = new Random();
         int max = 0, imax = 0;
         int array[][] = new int[12][8];
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < array.length; i++) {
             int sum = 0;
-            for (int j = 0; j < 8; j++) {
+            for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = rand.nextInt(50);
                 sum += array[i][j];
                 System.out.print(array[i][j] + "  ");
