4452f3381b5d9781f4df975bc244bbb7cf0b17e1
zhhhigalov <zhigalovmi@gmail.com>
2020-12-05 03:54:36 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,44 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car newCar = new Car();
+        newCar.setYearOfRelease(2020);
+        newCar.setColor("BLACK");
+        newCar.setModel("M320");
+
+        System.out.println("Год выпуска: " + newCar.getYearOfRelease());
+        System.out.println("Цвет машины: " + newCar.getColor());
+        System.out.println("Модель машины: " + newCar.getModel());
+    }
+}
+
+class Car {
+
+    private int yearOfRelease;
+    private String color;
+    private String model;
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,35 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike newMotorbike = new Motorbike(2021, "RED", "Bike001");
+
+        System.out.println("Год выпуска: " + newMotorbike.getYearOfRelease());
+        System.out.println("Цвет машины: " + newMotorbike.getColor());
+        System.out.println("Модель машины: " + newMotorbike.getModel());
+    }
+}
+
+class Motorbike {
+
+    Motorbike(int yearOfRelease, String color, String model) {
+        this.yearOfRelease = yearOfRelease;
+        this.color = color;
+        this.model = model;
+    }
+
+    private int yearOfRelease;
+    private String color;
+    private String model;
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
