ef5c4c1ac3a112e1a726b6cf6917eaf4926f22b5
zhhhigalov <zhigalovmi@gmail.com>
2020-12-14 21:40:24 +0300

    complete day12 tasks fixes

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -7,9 +7,10 @@ public class Task2 {
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i <= 350; i++) {
-            if(i>30 && i<300) continue;
-            else if (i % 2 == 0) list.add(i);
+            if (i % 2 == 0) list.add(i);
+            if (i == 30) i = 299;
         }
         System.out.println(list);
     }
