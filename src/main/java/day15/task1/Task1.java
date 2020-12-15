140f1196ab8dc935e34bbd17b1d7106261632cc1
zhhhigalov <zhigalovmi@gmail.com>
2020-12-15 23:02:34 +0300

    complete day15 tasks

--- src/main/java/day15/task1/Task1.java
+++ src/main/java/day15/task1/Task1.java
@@ -1,7 +1,25 @@
 package day15.task1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File("src/main/resources/shoes.csv"));
+        PrintWriter out = new PrintWriter("src/main/resources/missing_shoes.txt");
+
+        while (in.hasNextLine()) {
+            String str = in.nextLine();
+            String[] array = str.split(";");
+            if (Integer.parseInt(array[2]) == 0)
+                out.println(str);
+        }
+        in.close();
+        out.close();
+
 
     }
 }
