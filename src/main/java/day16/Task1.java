03e56c08d0f721ce1b2be572beec62427ece04ba
zhhhigalov <zhigalovmi@gmail.com>
2020-12-17 01:15:00 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File f = new File("numbers.txt");
+        printResult(f);
+    }
+
+    public static void printResult(File file) {
+        try {
+            Scanner in = new Scanner(file);
+            int counter = 1;
+            double sum = in.nextInt();
+            while (in.hasNextInt()) {
+                sum += in.nextInt();
+                counter++;
+            }
+            double result = sum / counter;
+            System.out.print(result + " --> ");
+            System.out.printf("%.3f", result);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,41 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        PrintWriter out1 = new PrintWriter(file1);
+        PrintWriter out2 = new PrintWriter(file2);
+        Random random = new Random();
+        double sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            int rand = random.nextInt(101);
+            out1.print(rand + " ");
+            sum += rand;
+            if (i % 20 == 0) {
+                out2.print(sum / 20 + " ");
+                sum = 0;
+            }
+        }
+        out1.close();
+        out2.close();
+        printResult(file2);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        double sumResult = 0;
+        Scanner in = new Scanner(file).useLocale(Locale.US);
+        while (in.hasNextDouble())
+            sumResult += in.nextDouble();
+        System.out.println((int) sumResult);
     }
 }
