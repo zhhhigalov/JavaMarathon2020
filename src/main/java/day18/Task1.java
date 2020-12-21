b4b2d0ad45055aebd8bf47bd80c5d9cffda7a87b
zhhhigalov <zhigalovmi@gmail.com>
2020-12-22 02:13:07 +0300

    complete day18 tasks

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,25 @@
 package day18;
 
 public class Task1 {
+
+    static int index;
+
     public static void main(String[] args) {
 
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers));
+        int[] numbers2 = {402, -50, -249, 215, 665, 311};
+        System.out.println(recursionSum(numbers2));
+        int[] numbers3 = {-402, -50, -249, -15, -665, -311};
+        System.out.println(recursionSum(numbers3));
+    }
+
+    public static int recursionSum(int[] a) {
+        if (a.length == 0) return 0;
+        if (a.length - 1 == index) {
+            index = 0;
+            return a[a.length - 1];
+        }
+        return a[index++] + recursionSum(a);
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,14 @@
 package day18;
 
 public class Task2 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(count7(-1123780));
+    }
+
+    public static int count7(int a) {
+        if (a == 0) return 0;
+        if (a % 10 == 7 || a % 10 == -7) return 1 + count7(a / 10);
+        else return count7(a / 10);
     }
 }
